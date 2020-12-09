d0293b56d73891748b2cc17810d6a73364ceff10
Alex Contreras <alexconh@gmail.com>
2020-12-08 21:05:43 -0600

    ADD weather exercise

---
+++ js/weather.js
@@ -0,0 +1,17 @@
+const WEATHER = prompt('¿Cómo está el clima?  \na. Soleado \nb. Lluvioso \nc. Nevando \nd. Nublado')
+switch (WEATHER){
+    case 'a':
+        console.log('You are so golden')
+        break
+    case 'b':
+        console.log('Rainy day')
+        break
+    case 'c': 
+        console.log('Frosty the snowman')
+        break
+    case 'd':
+        console.log('Catching clouds')
+    default:
+        const ALTERNATIVE = prompt('Descríbe tu día')
+        console.log(`${ALTERNATIVE}`)
+}

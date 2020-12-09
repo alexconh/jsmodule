833bffae6cef1c3b6f25a7a1f537cda4de9846d0
Alex Contreras <alexconh@gmail.com>
2020-12-09 01:46:13 -0600

    UPDATE weather.js with if-else if-else structure

--- js/weather.js
+++ js/weather.js
@@ -1,17 +1,32 @@
+//const WEATHER = prompt('¿Cómo está el clima?  \na. Soleado \nb. Lluvioso \nc. Nevando \nd. Nublado')
+//switch (WEATHER){
+//    case 'a':
+//        console.log('Dale play a Golden de Harry Styles')
+//        break
+//    case 'b':
+//        console.log('Podrías escuchar Rainy day de Coldplay')
+//        break
+//    case 'c': 
+//        console.log('Cantemos Frosty the snowman')
+//        break
+//    case 'd':
+//        console.log('Dale play a Catching clouds de Ed Sheppard')
+//    default:
+//        const WEATHER_DESCRIPTION = prompt('Descríbe tu día')
+//        console.log(`${WEATHER_DESCRIPTION}`)
+//}
+
 const WEATHER = prompt('¿Cómo está el clima?  \na. Soleado \nb. Lluvioso \nc. Nevando \nd. Nublado')
-switch (WEATHER){
-    case 'a':
-        console.log('You are so golden')
-        break
-    case 'b':
-        console.log('Rainy day')
-        break
-    case 'c': 
-        console.log('Frosty the snowman')
-        break
-    case 'd':
-        console.log('Catching clouds')
-    default:
-        const WEATHER_DESCRIPTION = prompt('Descríbe tu día')
-        console.log(`${WEATHER_DESCRIPTION}`)
+
+if (WEATHER === 'a') {
+    console.log('Dale play a Golden de Harry Styles')
+} else if (WEATHER === 'b') {
+    console.log('Podrías escuchar Rainy day de Coldplay')
+} else if (WEATHER === 'c') {
+    console.log('Cantemos Frosty the snowman')
+} else if (WEATHER === 'd') {
+    console.log('Dale play a Catching clouds de Ed Sheppard')
+} else {
+    const WEATHER_DESCRIPTION = prompt('¿Describime tu día?')
+    console.log(WEATHER_DESCRIPTION)
 }

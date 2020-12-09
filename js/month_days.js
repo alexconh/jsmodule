1e57da5f9e01f989f55544a130d6c8b60bb0741c
Alex Contreras <alexconh@gmail.com>
2020-12-09 02:37:02 -0600

    UPDATE month_days.js

--- js/month_days.js
+++ js/month_days.js
@@ -2,7 +2,7 @@ console.log('month_days')
 const MONTH = parseInt(prompt('Dame un número del 1 al 12'))
 if (MONTH == 2){
     console.log(`Al mes número ${MONTH} le corresponden 28 días`)
-} else if (MONTH<8 && MONTH != 2){
+} else if (MONTH<8){
     if (MONTH%2==0){
         console.log(`Al mes número ${MONTH} le corresponden 30 días`)
     } else {

8f66fb3f4cbcec31d52e3a7cc990f33ff36ea073
Alex Contreras <alexconh@gmail.com>
2020-12-09 02:06:45 -0600

    ADD exercise 3 (even_odd.js)

---
+++ js/even_odd.js
@@ -0,0 +1,7 @@
+console.log('even_odd')
+
+const NUMBER = parseInt(prompt('Dame un número del 1 al 100'))
+NUMBER%2==0 ? 
+    console.log(`${NUMBER} es un número par`) 
+    : 
+    console.log(`${NUMBER} es un número impar`)

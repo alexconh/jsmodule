00955ff3674395abb7f79d34ccfa613ec9ee4406
Alex Contreras <alexconh@gmail.com>
2020-12-11 22:22:51 -0600

    UPDATE exercises

--- js/functions_exercises.js
+++ js/functions_exercises.js
@@ -8,18 +8,10 @@ function tablas_de_multiplicar(string_name) {
 
 function is_palindrome(string_to_compare) {
     let reversed = ''
-    let string_aux = ''
     for (let i = string_to_compare.length - 1; i >= 0; i--) {
-        if(string_to_compare[i]!=' '){
         reversed += string_to_compare[i]
-        }
     }
-    for (let i = 0; i < string_to_compare.length; i++) {
-        if(string_to_compare[i]!=' '){
-        string_aux += string_to_compare[i]
-        }
-    }
-    if (string_aux === reversed) {
+    if (string_to_compare.replace(/ /g,'') === reversed.replace(/ /g,'')) {
         console.log(`La frase ${string_to_compare} es un palíndromo`)
     } else {
         console.log(`La frase ${string_to_compare} no es un palíndromo`)
@@ -27,10 +19,10 @@ function is_palindrome(string_to_compare) {
 }
 
 function type_of_data(one, two){
-    console.log(`el primer dato es:${typeof(one)}`)
-    console.log(`el segundo dato es:${typeof(two)}`)
+    console.log(`el primer dato es: ${typeof(one)}`)
+    console.log(`el segundo dato es: ${typeof(two)}`)
 }
 
 // tablas_de_multiplicar('987')
-// is_palindrome('anita lava la tina')
+is_palindrome('anita lava la tina')
 // type_of_data('anita lava la tina', 15)
